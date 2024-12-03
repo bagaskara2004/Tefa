c68f2678578f280746a24648482e38668f537ffa
bagaskara2004 <bagaskaraputra87@gmail.com>
2024-12-03 11:57:54 +0800

    Testing Api user

--- app/Database/Seeds/Pesanan.php
+++ app/Database/Seeds/Pesanan.php
@@ -12,39 +12,39 @@ class Pesanan extends Seeder
         $encrypter = \Config\Services::encrypter();
         $data = [
             [
-                "id_admin" => 1,
                 "id_status" => 1,
                 "nama_pemesan" => $encrypter->encrypt('kafir'),
                 "telp_pemesan" => $encrypter->encrypt('08900099998'),
                 "judul_pesanan" => 'Youtube',
                 "deskripsi_pesanan" => 'website media sosial',
+                "actived" => true,
                 "otp_pesanan" => $encrypter->encrypt('06d78c'),
             ],
             [
-                "id_admin" => 1,
                 "id_status" => 2,
                 "nama_pemesan" => $encrypter->encrypt('rosi'),
                 "telp_pemesan" => $encrypter->encrypt('08904229998'),
                 "judul_pesanan" => 'Race',
                 "deskripsi_pesanan" => 'website berita moto racing',
+                "actived" => true,
                 "otp_pesanan" => $encrypter->encrypt('78g22h'),
             ],
             [
-                "id_admin" => 1,
                 "id_status" => 3,
                 "nama_pemesan" => $encrypter->encrypt('dombel'),
                 "telp_pemesan" => $encrypter->encrypt('08598629998'),
                 "judul_pesanan" => 'Obit',
                 "deskripsi_pesanan" => 'aplication hack nasa',
+                "actived" => true,
                 "otp_pesanan" => $encrypter->encrypt('54ss6i'),
             ],
             [
-                "id_admin" => 1,
                 "id_status" => 4,
                 "nama_pemesan" => $encrypter->encrypt('wayan'),
                 "telp_pemesan" => $encrypter->encrypt('082734957348'),
                 "judul_pesanan" => 'E-learning',
                 "deskripsi_pesanan" => 'aplication dan web pembelajaran',
+                "actived" => true,
                 "otp_pesanan" => $encrypter->encrypt('98i223'),
             ],
         ];
